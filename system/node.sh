ddb8ae05b57d2e0e1a3d38949b26c7274175b155
Weslley Cavalcante <whenriquerc@gmail.com>
2016-11-15 22:02:17 -0200

    Fix - load nvm functions

--- system/node.sh
+++ system/node.sh
@@ -4,10 +4,17 @@
 # Contact rafaelcgstz@gmail.com
 #
 
-node.sh() {
+node.sh () {
 	clear;
-	wget -qO- -no-check-certificate https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-	nvm install node;
-	nvm use --default node
-	clear;
+	echo -e "====== Start NodeJS installer";
+
+	#Download NVM package
+	wget -qO- --no-check-certificate https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash;
+	. ~/.nvm/nvm.sh;
+	. ~/.profile;
+	. ~/.bashrc;
+
+	#Install Node LTS Version
+	nvm install --lts=Boron;
+	nvm alias default lts/boron;
 }
